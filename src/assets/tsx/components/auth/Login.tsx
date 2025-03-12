80884223396ae218030c9d7e623c8cc2d62667c6
Pemerintah <tangkawarowandika@gmail.com>
2025-03-12 23:59:35 +0800

    Selesai design untuk bagian Login

--- src/assets/tsx/components/auth/Login.tsx
+++ src/assets/tsx/components/auth/Login.tsx
@@ -1,26 +1,34 @@
 export default function Login() {
     return (
-        <aside>
-            <h2>Login</h2>
-            <div>
-                <div>
+        <form className="auth__login">
+            <h1>Login</h1>
+            <div className="auth__login__input">
+                <div className="auth__login__input__item">
                     <label htmlFor="username">Username</label>
                     <input type="text" id="username" />
                 </div>
-                <div>
+                <div className="auth__login__input__item">
                     <label htmlFor="email">Email</label>
                     <input type="text" id="email" />
                 </div>
-                <div>
+                <div className="auth__login__input__item">
                     <label htmlFor="password">Password</label>
-                    <input type="text" id="password" />
+                    <input type="password" id="password" />
                 </div>
-                <div>
+                <div className="auth__login__input__item">
                     <label htmlFor="confirm-password">Confirm Password</label>
-                    <input type="text" id="confirm-password" />
+                    <input type="password" id="confirm-password" />
                 </div>
             </div>
-            <div></div>
-        </aside>
+            <div className="auth__login__button">
+                <button className="auth__login__button__submit" type="submit">
+                    Login
+                </button>
+                <p>
+                    Dosen't have account yet?{" "}
+                    <button type="button">Sign In</button>
+                </p>
+            </div>
+        </form>
     );
 }
