973d8e614461d2aee590d1b7fe1febeddbfc2c41
Jean <jeanarby030108@gmail.com>
2025-04-08 17:00:11 +0700

    peningkatan pada penanganan error - /auth/status

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,39 +5,39 @@ const setupAuthListener = require("./utils/authListener");
 const Cookie = require("@hapi/cookie");
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 3000,
-        host: "localhost",
-        routes: {
-            cors: {
-                origin: process.env.CORS_ORIGIN.split(","),
-                credentials: true,
-                additionalHeaders: ["Authorization", "Content-Type"],
-                additionalExposedHeaders: ["Authorization"],
-            },
-        },
-    });
+  const server = Hapi.server({
+    port: 3000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: process.env.CORS_ORIGIN.split(","),
+        credentials: true,
+        additionalHeaders: ["Authorization", "Content-Type"],
+        additionalExposedHeaders: ["Authorization"],
+      },
+    },
+  });
 
-    await server.register(Cookie);
+  await server.register(Cookie);
 
-    server.auth.strategy("session", "cookie", {
-        cookie: {
-            name: "session",
-            password: process.env.COOKIE_PASSWORD,
-            isSecure: false,
-            isSameSite: "Strict",
-            isHttpOnly: true,
-            path: "/",
-            ttl: 24 * 60 * 60 * 1000,
-        },
-    });
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: "session",
+      password: process.env.COOKIE_PASSWORD,
+      isSecure: false,
+      isSameSite: "None",
+      isHttpOnly: true,
+      path: "/",
+      ttl: 24 * 60 * 60 * 1000,
+    },
+  });
 
-    server.route(authRoutes);
+  server.route(authRoutes);
 
-    setupAuthListener(server);
+  setupAuthListener(server);
 
-    await server.start();
-    console.log("Server running on %s", server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
 init();
